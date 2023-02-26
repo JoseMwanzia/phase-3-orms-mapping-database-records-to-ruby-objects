8b1ad7d5c8654971c90e476e4cb1ee060b635a0e
joseph mwanzia <makamwanzia@gmail.com>
2023-02-26 23:42:45 +0300

    Completed lab

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,33 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new.
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL 
+      SELECT * 
+      FROM songs
+    SQL
+    # we will make a call to our database using DB[:conn]. This DB hash is located in the config/environment.rb file:
+    # Notice that the value of the connection in this hash is actually a new instance of the SQLite3::Database class. This is how we will connect to our database. Our database instance responds to a method called execute that accepts raw SQL as a string.
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
